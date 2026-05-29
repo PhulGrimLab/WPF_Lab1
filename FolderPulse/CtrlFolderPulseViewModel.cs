d191c4228b96ca64fb9e2f2bf5c38e2dcabe4206
PhulGrimLab <phulgrimlab@gmail.com>
2026-05-29 23:09:07 +0900

    폴더 정보 표시 UI 추가, AnyCPU 삭제

--- FolderPulse/CtrlFolderPulseViewModel.cs
+++ FolderPulse/CtrlFolderPulseViewModel.cs
@@ -6,21 +6,65 @@ namespace FolderPulse
 {
     public class CtrlFolderPulseViewModel : ViewModelBase
     {
-        private string _tb_SelectedFolderPath;
+        private string _tb_SelectedFolderPath = string.Empty;
         public string Tb_SelectedFolderPath
         {
             get => _tb_SelectedFolderPath;
             set => SetProperty(ref _tb_SelectedFolderPath, value);
         }
 
+        private string _tb_ActivatedFolder = "-------------";
+        public string Tb_ActivatedFolder
+        {
+            get => _tb_ActivatedFolder;
+            set => SetProperty(ref _tb_ActivatedFolder, value);
+        }
+
+        private string _tb_MonitoringInterval = "-------------";
+        public string Tb_MonitoringInterval
+        {
+            get => _tb_MonitoringInterval;
+            set => SetProperty(ref _tb_MonitoringInterval, value);
+        }
+
+        private string _tb_FolderSize = "-------------";
+        public string Tb_FolderSize
+        {
+            get => _tb_FolderSize;
+            set => SetProperty(ref _tb_FolderSize, value);
+        }
+
+        private string _tb_SubFolderCount = "-------------";
+        public string Tb_SubFolderCount
+        {
+            get => _tb_SubFolderCount;
+            set => SetProperty(ref _tb_SubFolderCount, value);
+        }
+
+
         public ICommand Btn_ExplorerCMD { get; }
+        public ICommand Btn_ActionCMD { get; }
 
         public CtrlFolderPulseViewModel() 
         {
             Btn_ExplorerCMD = new RelayCommand(ExecuteExplorerCMD);
-
+            Btn_ActionCMD = new RelayCommand(ExecuteActionCMD);
         }
 
+        private void ExecuteActionCMD(object? parameter)
+        {
+            string SelectedItemName = string.Empty;
+
+            try
+            {
+                // 선택된 폴더 경로를 활성화 폴더 경로로 지정
+                Tb_ActivatedFolder = Tb_SelectedFolderPath;
+            }
+            catch (Exception ex)
+            {
+                LogEx.Error($"[ERROR] ExecuteActionCMD()-Exception: {ex.ToString()}");
+            }
+        }
 
         private void ExecuteExplorerCMD(object? parameter)
         {
