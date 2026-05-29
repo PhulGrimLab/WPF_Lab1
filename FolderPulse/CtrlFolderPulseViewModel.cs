8433909bf561733131635dd2970d045ee447f2e4
PhulGrimLab <phulgrimlab@gmail.com>
2026-05-29 15:21:48 +0900

    [폴더모니터링] 모니터링 콘트롤 추가, CommonDef, ViewModelBase 추가

---
+++ FolderPulse/CtrlFolderPulseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Lib.CommonDef;
+
+namespace FolderPulse
+{
+    public class CtrlFolderPulseViewModel : ViewModelBase
+    {
+        public ICommand Btn_ExplorerCMD { get; } = new RelayCommand(ExecuteExplorerCMD);
+    }
+}
--- Lib.CommonDef/CommonDef.cs
+++ Lib.CommonDef/CommonDef.cs
@@ -1,11 +1,153 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Lib.CommonDef
 {
+    public sealed class RelayCommand : ICommand, IDisposable
+    {
+        private Action<object?>? _execute;
+        private Func<object?, bool>? _canExecute;
+        private bool _disposed;
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+
+            _execute = _ => execute();
+            _canExecute = canExecute is null ? null : _ => canExecute();
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            _execute = null;
+            _canExecute = null;
+            _disposed = true;
+        }
+    }
+
+    public sealed class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T?> _execute;
+        private readonly Func<T?, bool>? _canExecute;
+
+        public RelayCommand(Action<T?> execute, Func<T?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(ConvertParameter(parameter)) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(ConvertParameter(parameter));
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private static T? ConvertParameter(object? parameter)
+        {
+            if (parameter is null)
+                return default;
+
+            if (parameter is T typed)
+                return typed;
+
+            throw new ArgumentException($"Invalid command parameter type. Expected {typeof(T).Name}.", nameof(parameter));
+        }
+    }
+
+    public sealed class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<Task> _executeAsync;
+        private readonly Func<bool>? _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<Task> executeAsync, Func<bool>? canExecute = null)
+        {
+            _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return !_isExecuting && (_canExecute?.Invoke() ?? true);
+        }
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+
+            try
+            {
+                await _executeAsync();
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
     public class CommonDef
     {
         private static readonly Lazy<CommonDef> _singleton = new Lazy<CommonDef>(() => new CommonDef());
