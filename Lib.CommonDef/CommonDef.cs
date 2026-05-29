902063c6ca8aaa6aab6bbbf163e9c5077e309a4c
PhulGrimLab <phulgrimlab@gmail.com>
2026-05-29 14:29:37 +0900

    폴더모니터링프로그램 최초 업로드

---
+++ Lib.CommonDef/CommonDef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.CommonDef
+{
+    public class CommonDef
+    {
+        private static readonly Lazy<CommonDef> _singleton = new Lazy<CommonDef>(() => new CommonDef());
+
+        public static CommonDef Instance => _singleton.Value;
+    }
+}
